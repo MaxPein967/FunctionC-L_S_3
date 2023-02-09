4c687d01bc8a7aae5c920483a2a5eb4021e2cd39
Sergei Vasiliev <9670105747@mail.ru>
2023-02-09 15:59:51 +0700

    задача 32 положительные числа заменить на отрицательные

--- C#_4/Program.cs
+++ C#_4/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 32: 
+//Напишите программу замена элементов
+//массива: положительные элементы замените
+//на соответствующие отрицательные, и 
+//наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+//int[] numbers = { -4, -8, 8, 2 };
+Console.Clear();
+int[] numbers = new int[12];
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(-9, 10);
+}
+Console.WriteLine(string.Join(" ", numbers));
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = numbers[i] * -1; // numbers[i]*=-1
+}
+Console.WriteLine(string.Join(" ",numbers));
