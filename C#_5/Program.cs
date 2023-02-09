40cd5e72ea9ee826d92364a423ac9634e52a31f3
Sergei Vasiliev <9670105747@mail.ru>
2023-02-09 16:01:05 +0700

    Задача 33 поиск числа в массиве

--- C#_5/Program.cs
+++ C#_5/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 33: 
+//Задайте массив. Напишите программу, 
+//которая определяет, присутствует ли 
+//заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+Console.Clear();
+int[] numbers = new int[10];
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(-20, 20);
+}
+Console.WriteLine(string.Join(" ", numbers));
+int find = 10;
+String Ans = "No";
+for (int i = 0; i < numbers.Length; i++)
+{
+    if(numbers[i] == find)
+    {
+        Ans = "Yes";
+        break;
+    }
+}
+Console.WriteLine(Ans);
+    
+
